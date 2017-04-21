56b444211659f67049a041b5743c70826de96332
AITE Meziane <meziane.aite@inria.fr>
2017-04-21 18:06:09 +0200

    Now if fba test < 0, print Flux for each cpds of the reaction

--- exploration/fba_test.py
+++ exploration/fba_test.py
@@ -56,5 +56,55 @@ def main():
         for r_id in blocked:
             r_id_decoded = convert_from_coded_id(r_id)[0]
             print('\t'+r_id_decoded+' ('+r_id+')')
+    
+    else:
+        biomassrxn = [rxn for rxn in model.reactions if rxn.objective_coefficient == 1.0][0]
+        biomassname = biomassrxn.id
+        metabolites = biomassrxn._metabolites
+        # print(metabolites)
+        # only append to the list compounds that are reactants
+        bms_metabolites_list = [x.id for x in metabolites]
+        bms_reactants = [x.id for x in metabolites if metabolites[x] < 0]
+        # for metabo in metabolites:
+        #     metabolites_list.append(metabo.id)
+    
+        #print(metabolites_list)
+        dict_output = {"positive":{},"negative":{}}
+        has_been_tested = []
+        for metabolite1 in bms_reactants:
+            if not metabolite1 in has_been_tested:
+                #lets create a copy of the initial model
+                model2 = model.copy()
+                #get the biomass reaction
+                biomassrxn2 = model2.reactions.get_by_id(biomassname) #R_Ec_biomass_iAF1260_core_59p81M
+                # empty the biomass metabolites
+                for m in bms_metabolites_list:
+                    biomassrxn2.pop(m)
+                #create a clean dict with only compound of interest and stoichio to -1 (reactant)
+                metabolitedict = {}
+                metabolitedict[metabolite1]=-1.0
+                # replace the biomass metabolites with only metabolite1 inside
+                biomassrxn2.add_metabolites(metabolitedict)
+                # print(biomassrxn2._metabolites)
+                #test flux balance analysis
+                #print(biomassrxn2._metabolites)
+                model2.optimize(solver='glpk')
+                if (model2.solution.f > 1e-5):
+                    dict_output["positive"][metabolite1] = model2.solution.f
+                else:
+                    dict_output["negative"][metabolite1] = model2.solution.f
+                has_been_tested.append(metabolite1)
+            else:
+                pass
+        for k,v in dict_output["positive"].items():
+            print("%s %s positive" %(k,v))
+        for k,v in dict_output["negative"].items():
+            print("%s %s negative" %(k,v))
+        print("%s/%s compounds with positive flux" %(len(dict_output["positive"].keys()), len(has_been_tested)))
+        print("%s/%s compounds with positive flux" %(len(dict_output["negative"].keys()), len(has_been_tested)))
+
+
+
+
 if __name__ == "__main__":
     main()
