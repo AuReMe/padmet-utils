ec23edbeef64999a18a67a13cbf5a89b20135409
AITE Meziane <meziane.aite@inria.fr>
2017-12-13 20:17:53 +0100

    adding script to manage compartment in padmet

--- management/update_compart.py
+++ management/update_compart.py
@@ -19,7 +19,9 @@ along with padmet-utils. If not, see <http://www.gnu.org/licenses/>.
 Description:
 
 usage:
-    change_compart.py --padmet=FILE --old=STR --new=STR [--output=FILE]
+    update_compart.py --padmet=FILE
+    update_compart.py --padmet=FILE --remove=STR [--output=FILE] [-v]
+    update_compart.py --padmet=FILE --old=STR --new=STR [--output=FILE] [-v]
 
 options:
     -h --help     Show help.
@@ -38,13 +40,31 @@ def main():
     old_compart = args["--old"]
     new_compart = args["--new"]
     new_padmet = args["--output"]
+    to_remove = args["--remove"]
+    verbose = args["-v"]
     if new_padmet is None:
         new_padmet = args["--padmet"]
     padmetSpec = PadmetSpec(padmet_file)
 
-    #create the file with compounds ids and the current compartment corresponding to 'compart'    
-    padmetSpec.change_compart(old_compart, new_compart)
-    padmetSpec.generateFile(new_padmet)            
+    if to_remove:
+        if "," in to_remove:
+            to_remove = to_remove.split(",")
+        else:
+            to_remove = [to_remove]
+        for compart in to_remove:
+            if verbose:
+                print("removing reaction(s) in compartment %s" %compart)
+            padmetSpec.delCompart(compart, verbose)
+        padmetSpec.generateFile(new_padmet)            
+    elif old_compart and new_compart:
+        if verbose:
+            print("Converting compartment %s to %s" %(old_compart, new_compart))
+        padmetSpec.change_compart(old_compart, new_compart, verbose)
+        padmetSpec.generateFile(new_padmet)            
+    else:
+        if verbose:
+            print("List of compartments")
+        print(list(padmetSpec.get_all_compart()))
 
 if __name__ == "__main__":
     main()
