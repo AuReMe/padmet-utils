3dd4afb4c6a0a3b24fc25d412d4ecb7d1fbc3aa7
AITE Meziane <meziane.aite@inria.fr>
2018-01-08 19:52:05 +0100

    enhancing flux analysis

--- exploration/flux_analysis.py
+++ exploration/flux_analysis.py
@@ -21,13 +21,16 @@ Run flux balance analyse with cobra package. If the flux is >0. Run also FVA
 and return result in standard output
 
 usage:
-    flux_analysis.py --sbml=FILE [--full_report] --seeds=FILE --targets=FILE
+    flux_analysis.py --sbml=FILE [--seeds=FILE] [--targets=FILE]
+    flux_analysis.py --sbml=FILE --seeds=FILE --targets=FILE
     flux_analysis.py --sbml=FILE --all_species
-    flux_analysis.py --sbml=FILE --species=FILE
 
 option:
     -h --help    Show help.
     --sbml=FILE    pathname to the sbml file to test for fba and fva.
+    --seeds=FILE    pathname to the sbml file containing the seeds (medium).
+    --targets=FILE    pathname to the sbml file containing the targets.
+    --all_species    allow to make FBA on all the metabolites of the given model.
 """
 from padmet.sbmlPlugin import convert_from_coded_id
 from cobra import *
@@ -40,8 +43,9 @@ def main():
     sbml_file = args["--sbml"]
     seeds_file = args["--seeds"]
     targets_file = args["--targets"]
-
-    targets = create_cobra_model_from_sbml_file(targets_file).metabolites
+    if targets_file:
+        targets = create_cobra_model_from_sbml_file(targets_file).metabolites
+    all_species = args["--all_species"]
     model=create_cobra_model_from_sbml_file(sbml_file)
 
     try:
@@ -59,16 +63,31 @@ def main():
     print("Number of compounds: %s" %len(real_metabolites))
     print("Number of reactions: %s" %len(model.reactions))
     print("Number of genes: %s" %len(model.genes))
-    print("ratio rxn_ga/rxns: %s%%" %(100*len(rxn_with_ga)/len(model.reactions)))
-    cmd = "menecheck.py -d %s -s %s -t %s" %(sbml_file, seeds_file, targets_file)
-    out = subprocess.check_output(["/bin/bash", "-i", "-c", cmd])
-    prod_targets = (i for i in out.splitlines() if i.endswith(" producible targets:")).next()[:-1]
-    print("%s targets" %(len(targets)))
-    print(prod_targets)
-    fba_given_species(targets, model)
+    print("Ratio rxn with genes/rxns: %s%%" %(100*len(rxn_with_ga)/len(model.reactions)))
+    
+    #
+    if seeds_file and targets_file:
+        print("#############")
+        print("Analyzing targets")
+        print("#Topological analysis")
+        cmd = "menecheck.py -d %s -s %s -t %s" %(sbml_file, seeds_file, targets_file)
+        out = subprocess.check_output(["/bin/bash", "-i", "-c", cmd])
+        #prod_targets = (i for i in out.splitlines() if i.endswith(" producible targets:")).next()[:-1]
+        print("Number of targets: %s" %(len(targets)))
+        print out
+        print("#Flux Balance Analysis")
+        fba_on_targets(targets, model)
+    if all_species:
+        targets = model.metabolites
+        print("#Flux Balance Analysis on all model metabolites (long process...)")
+        fba_on_targets(targets, model)
+    print("#############")
+    print("Computing optimization")
     solution= model.optimize()
     print("Testing reaction %s" %biomassname)
-    print( 'growth rate: %s\nStatus: %s.' %(solution.objective_value, solution.status))
+    print("Growth rate: %s" %solution.objective_value)
+    print("Status: %s" %solution.status)
+    model.summary()
     if (solution.objective_value > 1e-5):
         blocked = flux_analysis.find_blocked_reactions(model, model.reactions)
         essRxns = flux_analysis.find_essential_reactions(model)
@@ -79,48 +98,39 @@ def main():
         print('\tEssential reactions: %s' %len(essRxns))
         print('\tEssential genes: %s' %len(essGenes))
     
-    else:
-        # print(metabolites)
-        # only append to the list compounds that are reactants
-        bms_reactants = biomassrxn.reactants
-        biomassrxn.objective_coefficient = 0
-        # for metabo in metabolites:
-        #     metabolites_list.append(metabo.id)
-    
-        #print(metabolites_list)
-        dict_output = {"positive":{},"negative":{}}
-        for metabolite in bms_reactants:
-            #lets create a copy of the initial model
-            model2 = model.copy()
-            #get the biomass reaction
-            biomassrxn2 = model2.reactions.get_by_id(biomassname) #R_Ec_biomass_iAF1260_core_59p81M
-            biomassrxn2.objective_coefficient = 1
-            # empty the biomass metabolites
-            for i in range(len(bms_reactants)):
-                biomassrxn2.reactants.pop(i)
-            #create a clean dict with only compound of interest and stoichio to -1 (reactant)
-            metabolitedict = {}
-            metabolitedict[metabolite]=-1.0
-            # replace the biomass metabolites with only metabolite1 inside
-            biomassrxn2.add_metabolites(metabolitedict)
-            # print(biomassrxn2._metabolites)
-            #test flux balance analysis
-            #print(biomassrxn2._metabolites)
-            solution2 = model2.optimize()
-            if (solution2.objective_value > 1e-5):
-                dict_output["positive"][metabolite] = solution2.objective_value
-            else:
-                dict_output["negative"][metabolite] = solution2.objective_value
+    #get biomass rxn reactants
+    bms_reactants = dict([(k,v) for k,v in biomassrxn.metabolites.items() if v < 0])
+    bms_products = dict([(k,v) for k,v in biomassrxn.metabolites.items() if v > 0])
+    dict_output = {"positive":{},"negative":{}}
+    #for each metabolite in reactant, create a biomass rxn with only this metabolite in reactants
+    biomassrxn.objective_coefficient = 0.0
+    for reactant, stoich in bms_reactants.items():
+        test_rxn = Reaction("test_rxn")
+        test_rxn.lower_bound = 0
+        test_rxn.upper_bound = 1000
+        metabolitedict = dict(bms_products)
+        metabolitedict.update({reactant:stoich})
 
-        for k,v in dict_output["positive"].items():
-            print("%s %s positive" %(k,v))
-        for k,v in dict_output["negative"].items():
-            print("%s %s negative" %(k,v))
-        print("%s/%s compounds with positive flux" %(len(dict_output["positive"].keys()), len(bms_reactants)))
-        print("%s/%s compounds with negative flux" %(len(dict_output["negative"].keys()), len(bms_reactants)))
+        model.add_reaction(test_rxn)
+        test_rxn.add_metabolites(metabolitedict)
+        test_rxn.objective_coefficient = 1.0
+
+        solution = model.optimize()
+        if (solution.objective_value > 1e-5):
+            dict_output["positive"][reactant] = solution.objective_value
+        else:
+            dict_output["negative"][reactant] = solution.objective_value
+        model.remove_reactions([test_rxn])
+    print("%s/%s compounds with positive flux" %(len(dict_output["positive"].keys()), len(bms_reactants)))
+    print("%s/%s compounds with negative flux" %(len(dict_output["negative"].keys()), len(bms_reactants)))
+
+    for k,v in dict_output["positive"].items():
+        print("%s %s %s positive" %(k, convert_from_coded_id(k.id)[0]+"_"+convert_from_coded_id(k.id)[2], v))
+    for k,v in dict_output["negative"].items():
+        print("%s %s %s negative" %(k, convert_from_coded_id(k.id)[0]+"_"+convert_from_coded_id(k.id)[2], v))
 
 
-def fba_given_species(allspecies, model):
+def fba_on_targets(allspecies, model):
     dict_output = {"positive":{},"negative":{}}
     for species in allspecies:
         #lets create a copy of the initial model
@@ -140,14 +150,13 @@ def fba_given_species(allspecies, model):
             dict_output["positive"][species] = solution.objective_value
         else:
             dict_output["negative"][species] = solution.objective_value
-    """
-    for k,v in dict_output["positive"].items():
-        print("%s %s positive" %(k,v))
-    for k,v in dict_output["negative"].items():
-        print("%s %s negative" %(k,v))
-    """
     print("%s/%s compounds with positive flux" %(len(dict_output["positive"].keys()), len(allspecies)))
     print("%s/%s compounds with negative flux" %(len(dict_output["negative"].keys()), len(allspecies)))
+
+    for k,v in dict_output["positive"].items():
+        print("%s %s %s positive" %(k, convert_from_coded_id(k.id)[0]+"_"+convert_from_coded_id(k.id)[2], v))
+    for k,v in dict_output["negative"].items():
+        print("%s %s %s negative" %(k, convert_from_coded_id(k.id)[0]+"_"+convert_from_coded_id(k.id)[2], v))
     
 
 if __name__ == "__main__":
