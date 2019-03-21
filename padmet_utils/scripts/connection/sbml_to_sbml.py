c25834bf18514c0f784bbead16b791312ddfd5dd
Arnaud Belcour <arnaud.belcour@irisa.fr>
2019-03-21 11:53:42 +0100

    Replace sbml version with sbml level.

--- padmet_utils/scripts/connection/sbml_to_sbml.py
+++ padmet_utils/scripts/connection/sbml_to_sbml.py
@@ -28,14 +28,14 @@ def run_sbml_to_sbml(multiprocess_data):
     """Turn sbml to sbml.
 
     Args:
-        multiprocess_data (dictionary): pathname to species sbml file, pathname to output sbml file, new sbml version level
+        multiprocess_data (dictionary): pathname to species sbml file, pathname to output sbml file, new sbml level
 
     Returns:
         bool: True if sbml file exists
     """
     padmet = from_sbml_to_padmet(sbml=multiprocess_data['sbml_file'], db=None, version=None,
                                 padmetSpec_file=None, source_tool=None, source_category=None, source_id=None, padmetRef_file=None, mapping=None, verbose=None)
-    padmet_to_sbml(padmet, multiprocess_data['sbml_output_file'], sbml_lvl=multiprocess_data['new_sbml_version'], verbose=False)
+    padmet_to_sbml(padmet, multiprocess_data['sbml_output_file'], sbml_lvl=multiprocess_data['new_sbml_level'], verbose=False)
 
     if multiprocess_data['sbml_output_file'] and not os.access(multiprocess_data['sbml_output_file'], os.W_OK):
         try:
@@ -48,24 +48,24 @@ def run_sbml_to_sbml(multiprocess_data):
         return True
 
 
-def from_sbml_to_sbml(input_sbml, output_sbml, new_sbml_version, cpu):
+def from_sbml_to_sbml(input_sbml, output_sbml, new_sbml_level, cpu):
     """Turn sbml to sbml.
 
     Args:
         input_sbml (string): pathname to species sbml file/folder
         output_sbml (string): pathname to output sbml file/folder
-        new_sbml_version (int): new sbml version level
+        new_sbml_level (int): new sbml level
         cpu (int): number of cpu
 
     Returns:
         output_sbml (string): pathname to output sbml file/folder
     """
     try:
-        new_sbml_version = int(new_sbml_version)
+        new_sbml_level = int(new_sbml_level)
     except:
         print('SBML level must be an int.')
 
-    if new_sbml_version not in [2, 3]:
+    if new_sbml_level not in [2, 3]:
         sys.exit('New SBML level must be 2 or 3.')
 
     sbml_to_sbml_pool = Pool(processes=cpu)
@@ -81,14 +81,14 @@ def from_sbml_to_sbml(input_sbml, output_sbml, new_sbml_version, cpu):
             multiprocess_data = {}
             multiprocess_data['sbml_file'] = input_sbml + '/' + sbml_file
             multiprocess_data['sbml_output_file'] = output_sbml + '/' + sbml_file
-            multiprocess_data['new_sbml_version'] = new_sbml_version
+            multiprocess_data['new_sbml_level'] = new_sbml_level
             multiprocess_datas.append(multiprocess_data)
 
     elif os.path.isfile(input_sbml):
         multiprocess_data = {}
         multiprocess_data['sbml_file'] = input_sbml
         multiprocess_data['sbml_output_file'] = output_sbml
-        multiprocess_data['new_sbml_version'] = new_sbml_version
+        multiprocess_data['new_sbml_level'] = new_sbml_level
         multiprocess_datas.append(multiprocess_data)
 
     sbml_checks = sbml_to_sbml_pool.map(run_sbml_to_sbml, multiprocess_datas)
@@ -106,14 +106,14 @@ def call_sbml_to_sbml():
     args = docopt.docopt(__doc__)
     input_sbml = args["--input"]
     output_sbml = args["--output"]
-    new_sbml_version = args["--new_sbml_lvl"]
+    new_sbml_level = args["--new_sbml_lvl"]
 
     if args["--cpu"]:
         cpu = args['--cpu']
     else:
         cpu = 1
 
-    from_sbml_to_sbml(input_sbml, output_sbml, new_sbml_version, cpu)
+    from_sbml_to_sbml(input_sbml, output_sbml, new_sbml_level, cpu)
 
 if __name__ == "__main__":
     call_sbml_to_sbml()
