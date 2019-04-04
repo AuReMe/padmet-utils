951801ecaeae490e62055fef40ed1460ccacf3f8
AITE Meziane <meziane.aite@inria.fr>
2019-04-04 16:02:19 +0200

    get_pwy_from_rxn package compatible

--- padmet_utils/exploration/compare_sbml_padmet.py
+++ padmet_utils/exploration/compare_sbml_padmet.py
@@ -21,13 +21,31 @@ import docopt
 def main():
     args = docopt.docopt(__doc__)
     sbml_file = args["--sbml"]
-    padmetSpec = PadmetSpec(args["--padmet"])
-    
     reader = libsbml.SBMLReader()
     document = reader.readSBML(sbml_file)
-    model = document.getModel()
-    sbml_listOfReactions = set([sp.convert_from_coded_id(r.getId())[0] for r in model.getListOfReactions()])
-    padmet_reaction = set([node.id for node in padmetSpec.dicOfNode.values() if node.type == "reaction"])
+    for i in range(document.getNumErrors()):
+        print(document.getError(i).getMessage())
+    sbml_model = document.getModel()
+
+    padmet = PadmetSpec(args["--padmet"])
+    compare_sbml_padmet(sbml_model, padmet)
+    
+def compare_sbml_padmet(sbml_model, padmet):
+    """
+    compare reactions ids in sbml vs padmet, return nb of reactions in both
+    and reactions id not in sbml or not in padmet
+
+    Parameters
+    ----------
+    padmet: padmet.classes.PadmetSpec
+        padmet to udpate
+    sbml_file: libsbml.document.model
+        sbml model
+
+    """
+
+    sbml_listOfReactions = set([sp.convert_from_coded_id(r.getId())[0] for r in sbml_model.getListOfReactions()])
+    padmet_reaction = set([node.id for node in padmet.dicOfNode.values() if node.type == "reaction"])
     diff = sbml_listOfReactions.difference(padmet_reaction)
     diff_inv = padmet_reaction.difference(sbml_listOfReactions)
     
