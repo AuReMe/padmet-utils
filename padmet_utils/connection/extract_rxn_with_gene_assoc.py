b318f95be17fdd059f0d7df6ad1e9740e44b580b
AITE Meziane <meziane.aite@inria.fr>
2019-04-05 13:23:51 +0200

    extract_rxn_with_gene_assoc.py package compatible

--- padmet_utils/connection/extract_rxn_with_gene_assoc.py
+++ padmet_utils/connection/extract_rxn_with_gene_assoc.py
@@ -24,10 +24,30 @@ def main():
     args = docopt.docopt(__doc__)
     sbml_file = args["--sbml"]
     output = args["--output"]
+    verbose = args["-v"]
     reader = libsbml.SBMLReader()
-    document = reader.readSBML(sbml_file)
-    model = document.getModel()
-    listOfReactions = model.getListOfReactions()
+    sbml_document = reader.readSBML(sbml_file)
+    for i in range(sbml_document.getNumErrors()):
+        print(sbml_document.getError(i).getMessage())
+
+    extract_rxn_with_gene_assoc(sbml_document, output, verbose)
+
+
+def extract_rxn_with_gene_assoc(sbml_document, output, verbose=False):
+    """
+    From a given sbml document, create a sbml with only the reactions associated to a gene.
+    Need for a reaction, in section 'note', 'GENE_ASSOCIATION': ....
+
+    Parameters
+    ----------
+    sbml_file: libsbml.document
+        sbml document
+    output: str
+        pathname of the output sbml
+    """
+    sbml_model = sbml_document.getModel()
+
+    listOfReactions = sbml_model.getListOfReactions()
     
     reactions_to_remove = []
     for reaction in listOfReactions:
@@ -36,7 +56,7 @@ def main():
     for rId in reactions_to_remove:
         listOfReactions.remove(rId)
     
-    libsbml.writeSBMLToFile(document, output)
+    libsbml.writeSBMLToFile(sbml_document, output)
 
 if __name__ == "__main__":
     main()
