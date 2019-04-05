8db11ac806245a0e026fbca0e03f023b952d932a
AITE Meziane <meziane.aite@inria.fr>
2019-04-05 15:31:11 +0200

    extract_orthofinder.py package compatible

--- padmet_utils/connection/extract_orthofinder.py
+++ padmet_utils/connection/extract_orthofinder.py
@@ -1,24 +1,57 @@
 # -*- coding: utf-8 -*-
 """
 Description:
-    #todo
+    After running orthofinder on n fasta file, read the output file 'Orthogroups.csv'
+    Require a folder 'orthology_based_folder' with this archi:
+    \model_a
+        model_a.sbml
+    \model_b
+        model_b.sbml
+    And the name of the studied organism 'study_id'
+    1. Read the orthogroups file, extract orthogroups in dict 'all_orthogroups', and all org names
+    2. In orthology folder search for sbml files 'extension = .sbml'
+    3. For each models regroup all information in a dict dict_data:
+        {'study_id': study_id,
+        'model_id' : model_id,
+        'sbml_template': path to sbml of model',
+        'output': path to the output sbml,
+        'verbose': bool, if true print information
+        }
+        The output is by default: output_orthofinder_from_'model_id'.sbml
+    4. Store all previous dict_data in a list all_dict_data
+    5. iter on dict from all_dict_data and use function dict_data_to_sbml
+
+    Use a dict of data dict_data and dict of orthogroups dict_orthogroup to create sbml files.
+    dict_data and dict_orthogroup are obtained with fun orthofinder_to_sbml
+    6./ Read dict_orthogroups and check if model associated to dict_data and study org share orthologue
+    7./ Read sbml of model, parse all reactions and get genes associated to reaction.
+    8./ For each reactions:
+        Parse genes associated to sub part (ex: (gene-a and gene-b) or gene-c) = [(gene-a,gene-b), gene-c]
+        Check if study org have orthologue with at least one sub part (gene-a, gene-b) or gene-c
+        if yes: add the reaction to the new sbml and change genes ids by study org genes ids
+        Create the new sbml file.
     
 ::
    
     usage:
-        extract_orthofinder --orthology_based=DIR --orthgroups=FILE --study_id=STR --output=DIR [--mnx_rxn=FILE] [--mnx_cpd=FILE] [-v]
+        extract_orthofinder --orthology_based=DIR --orthgroups=FILE --study_id=STR --output=DIR [-v]
 
     option:
         -h --help    Show help.
+        --orthology_based=DIR   Folder with sub folder named as models name within sbml file name as model_name.sbml
+        --orthgroups=FILE   Output file of Orthofinder run Orthogroups.csv
+        --study_id   name of the studied organism
+        --output=DIR   folder where to create all sbml output files
         -v   print info
+
 """
 import re
 import csv
 import libsbml
 import docopt
 from padmet.utils import sbmlPlugin as sp
+from padmet.utils import gbr
 import os
-import gbr.gbr as gbr_compile
 
 
 def main():
@@ -26,13 +59,49 @@ def main():
     verbose = args["-v"]
     orthology_based_folder = args["--orthology_based"]
     orthogroups_file = args["--orthogroups"]
-    output_folder = args["--output"]
+    output = args["--output"]
     study_id = args["--study_id"]
 
-    orthofinder_to_sbml(orthogroups_file, orthology_based_folder, output_folder, study_id, verbose)
+    orthofinder_to_sbml(orthogroups_file, orthology_based_folder, output, study_id, verbose)
  
     
-def orthofinder_to_sbml(orthogroups_file, orthology_based_folder, output_folder, study_id, verbose = False):
+def orthofinder_to_sbml(orthogroups_file, orthology_based_folder, output, study_id, verbose = False):
+    """
+    After running orthofinder on n fasta file, read the output file 'Orthogroups.csv'
+    Require a folder 'orthology_based_folder' with this archi:
+    \model_a
+        model_a.sbml
+    \model_b
+        model_b.sbml
+    And the name of the studied organism 'study_id'
+    1. Read the orthogroups file, extract orthogroups in dict 'all_orthogroups', and all org names
+    2. In orthology folder search for sbml files 'extension = .sbml'
+    3. For each models regroup all information in a dict dict_data:
+        {'study_id': study_id,
+        'model_id' : model_id,
+        'sbml_template': path to sbml of model',
+        'output': path to the output sbml,
+        'verbose': bool, if true print information
+        }
+        The output is by default: output_orthofinder_from_'model_id'.sbml
+    4. Store all previous dict_data in a list all_dict_data
+    5. iter on dict from all_dict_data and use function dict_data_to_sbml
+    This function will create a sbml from each model and conserve only reactions associated to ortholog genes
+    For more information read the doc of func dict_data_to_sbml
+
+    Parameters
+    ----------
+    orthogroups_file: str
+        path of Orthofinder output file 'Orthogroups.csv'
+    orthology_based_folder: str
+        path of folder with model's sbml
+    output: str
+        pathname of the output folder of all sbml extracted
+    study_id: str
+        name of the studied organism
+    verbose: bool
+        if True print information
+    """
     if verbose:
         print("Parsing Orthofinder output %s" %orthogroups_file)
     #k=orthogroup_id, v = {k = name, v = set of genes}
@@ -57,14 +126,14 @@ def orthofinder_to_sbml(orthogroups_file, orthology_based_folder, output_folder,
     if verbose:
         print("Start sbml creation...")
     all_dict_data = []
-    if not os.path.exists(output_folder):
+    if not os.path.exists(output):
         if verbose:
-            print("\tCreating folder %s" %output_folder)
-        os.makedirs(output_folder)
+            print("\tCreating folder %s" %output)
+        os.makedirs(output)
     all_models = all_orgs - set([study_id])
     for model_id in all_models:
         output_name = "output_orthofinder_from_{0}.sbml".format(model_id)
-        output = os.path.join(output_folder, output_name)
+        output = os.path.join(output, output_name)
         sbml_template = all_model_sbml[model_id]
         dict_data = {'study_id': study_id, 'model_id': model_id, 'sbml_template': sbml_template, 'output': output, 'verbose':verbose}
         all_dict_data.append(dict_data)
@@ -73,9 +142,31 @@ def orthofinder_to_sbml(orthogroups_file, orthology_based_folder, output_folder,
         dict_data_to_sbml(dict_data, dict_orthogroup)
 
 
-def dict_data_to_sbml(dict_data, dict_orthogroup):
+def dict_data_to_sbml(dict_data, dict_orthogroup, strict_match=True):
     """
-    dict_orthogroup: global var
+    Use a dict of data dict_data and dict of orthogroups dict_orthogroup to create sbml files.
+    dict_data and dict_orthogroup are obtained with fun orthofinder_to_sbml
+    1./ Read dict_orthogroups and check if model associated to dict_data and study org share orthologue
+    2./ Read sbml of model, parse all reactions and get genes associated to reaction.
+    3./ For each reactions:
+        Parse genes associated to sub part (ex: (gene-a and gene-b) or gene-c) = [(gene-a,gene-b), gene-c]
+        Check if study org have orthologue with at least one sub part (gene-a, gene-b) or gene-c
+        if yes: add the reaction to the new sbml and change genes ids by study org genes ids
+    4./ Create the new sbml file.
+    
+    Parameters
+    ----------
+    dict_data: dict
+        {'study_id': study_id,
+        'model_id' : model_id,
+        'sbml_template': path to sbml of model',
+        'output': path to the output sbml,
+        'verbose': bool, if true print information
+        }
+    dict_orthogroup: dict
+        k=orthogroup_id, v = {k = name, v = set of genes}
+    verbose: bool
+        if True print information
     """
     #dict_data = {'study_name':'', 'o_compare_name': '', sbml_template':'', 'output':''}
     study_id = dict_data['study_id']
@@ -83,10 +174,6 @@ def dict_data_to_sbml(dict_data, dict_orthogroup):
     sbml_template = dict_data['sbml_template']
     output = dict_data['output']
     verbose = dict_data.get('verbose')
-    if os.path.isfile(output):
-        if verbose:
-            print("*{0} is already created, skip".format(os.path.basename(output)))
-        return
     if verbose:
         print("*Extracting orthology data to create sbml of {0} from {1}".format(study_id, model_id))
 
@@ -124,11 +211,12 @@ def dict_data_to_sbml(dict_data, dict_orthogroup):
         ga_for_gbr = re.sub(r" or " , "|", ga)
         ga_for_gbr = re.sub(r" and " , "&", ga_for_gbr)
         ga_for_gbr = re.sub(r"\s" , "", ga_for_gbr)
-        #ga_subsets = eval(subprocess.check_output("python3 grammar-boolean-rapsody.py "+ga_for_gbr, shell=True))
         if re.findall("\||&", ga_for_gbr):
-            to_compare_ga_subsets = list(gbr_compile.compile_input(ga_for_gbr))
+            to_compare_ga_subsets = list(gbr.compile_input(ga_for_gbr))
         else:
-            to_compare_ga_subsets = ga_for_gbr
+            ga_for_gbr = re.sub(r"\(|\)" , "", ga_for_gbr)
+            to_compare_ga_subsets = list([ga_for_gbr])
+        
         study_ga_subsets = []
         """
         to_compare_ga_subsets = [('a','c','d'),('c',)]
