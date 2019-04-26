50199a912ce992c545a52c956d08c8480cf0596d
AITE Meziane <meziane.aite@inria.fr>
2019-04-26 15:52:30 +0200

    update sbml_to_curation_form for package issue

--- padmet_utils/connection/sbml_to_curation_form.py
+++ padmet_utils/connection/sbml_to_curation_form.py
@@ -1,95 +1,130 @@
 # -*- coding: utf-8 -*-
 """
 Description:
-    extract all reactions from a sbml file to the form used in aureme for curation.
+    extract 1 reaction (if rxn_id) or a list of reactions (if rxn_file) 
+    from a sbml file to the form used in aureme for curation.
+    For example use this script to extract specific missing reaction of a model to
+    a just created metabolic network.
 
 ::
 
     usage:
-        sbml_to_curation_form.py --sbml=FILE --output=FILE --comment=STR [--rxn_id=ID]
+        sbml_to_curation_form.py --sbml=FILE --output=FILE --rxn_id=ID [--comment=STR] [--extract-gene] [-v]
+        sbml_to_curation_form.py --sbml=FILE --output=FILE --rxn_file=FILE [--comment=STR] [--extract-gene] [-v]
     
     options:
         -h --help     Show help.
         --sbml=FILE    path of the sbml.
         --output=FILE    form containing the reaction extracted, form used for manual curation in aureme.
-        --rxn_id=FILE    id of one reaction or n reactions sep by ';', if None try to extract the reaction with objective coefficient == 1.
-        --comment=STR    comment associated to the reactions in the form. Used to track sources of curation in aureme.
+        --rxn_id=FILE    id of one reaction to extract
+        --rxn_file=FILE    file of reactions ids to extract, 1 id by line.
+        --extract-gene    If true, extract also genes associated to reactions.
+        --comment=STR    comment associated to the reactions in the form. Used to track sources of curation in aureme [default: "N.A"].
+        -v   print info
+
 """
 
 import docopt
-from cobra.io.sbml import create_cobra_model_from_sbml_file
-from padmet.utils.sbmlPlugin import convert_from_coded_id
+import libsbml
+from padmet.utils.sbmlPlugin import convert_from_coded_id, parseNotes
 
 def main():
     args = docopt.docopt(__doc__)
     sbml_file = args["--sbml"]
-    rxn_id = args["--rxn_id"]
+    if args["--rxn_id"]:
+        rxn_list = [args["--rxn_id"]]
+    if args["--rxn_file"]:
+        with open(args["--rxn_id"], 'r') as f:
+            rxn_list = f.read.splitlines()
     output = args["--output"]
     comment = args["--comment"]
+    verbose = args["-v"]
+    extract_gene = args["--extract-gene"]
+    sbml_to_curation(sbml_file, rxn_list, output, extract_gene=extract_gene, comment=comment, verbose=verbose)
 
-    model=create_cobra_model_from_sbml_file(sbml_file)
 
-    if not args["--rxn_id"]:
-        try:
-            rxn = next(rxn for rxn in model.reactions if rxn.objective_coefficient == 1.0)
-        except StopIteration:
-            print("No reaction id given and no reaction with obj coefficient to 1.0, enable to get the reaction")
-            exit()
-    else:
-        rxns = args["--rxn_id"].split(";")
-        rxns_list = []
-        for rxn_id in rxns:
-            try:
-                rxn = model.reactions.get_by_id(rxn_id)
-            except KeyError:
-                try:
-                    rxn_id = rxn_id[2:]
-                    rxn = model.reactions.get_by_id(rxn_id)
-                except KeyError:
-                    print("Can't find reaction %s" %rxn_id)
-                    exit()
-            rxns_list.append(rxn)
+def sbml_to_curation(sbml_file, rxn_list, output, extract_gene=False, comment="N.A", verbose=False):
+    """
+    Read a sbml file, check if each reaction ids are in the sbml, if no, raise ValueError
+    Then create the form. this form can then be used with manual_curation.py
+
+    Parameters
+    ----------
+    sbml_file: str
+        path to sbml file
+    rxn_list: list
+        list of reaction id, ids must be identic as in the sbml, carrefull to encoded ids.
+    output: str
+        path to the form to create
+    extract_gene: bool
+        if true extract genes association
+    comment: str
+        Comment why the reaction will be added in the network for traceability.
+    verbose: bool
+        if True print information
     
+    """
+    reader = libsbml.SBMLReader()
+    document = reader.readSBML(sbml_file)
+    for i in range(document.getNumErrors()):
+        print(document.getError(i).getMessage())
+    model = document.getModel()
+    listOfReactions = model.getListOfReactions()
+    #check if reactions id are in model.
+    if verbose:
+        print("Check if reaction(s) are in sbml file")
+    for rxn_id in rxn_list:
+        if rxn_id in [r.id for r in listOfReactions]:
+            if verbose:
+                print("reaction %s found" %rxn_id)
+        else:
+            raise ValueError("/!\ reaction %s not found" %rxn_id)
+            
+    #create form output
     with open(output, 'w') as f:
-        for rxn in rxns_list:
-            rxn_id = convert_from_coded_id(rxn.id)[0]
-            line = ["reaction_id",rxn_id]
+        for rxn_id in rxn_list:
+            rxn_sbml = listOfReactions.getElementBySId(rxn_id)
+            rxn_id_decoded = convert_from_coded_id(rxn_id)[0]
+            if verbose:
+                print("extracting reaction %s, decoded id as %s" %(rxn_id, rxn_id_decoded))
+            line = ["reaction_id",rxn_id_decoded]
             line = "\t".join(line)+"\n"
             f.write(line)
             line = ["comment",comment]
             line = "\t".join(line)+"\n"
             f.write(line)
-            if rxn.reversibility:
+            if rxn_sbml.reversible:
                 line = ["reversible","true"]
             else:
                 line = ["reversible","false"]
             line = "\t".join(line)+"\n"
             f.write(line)
             #check if have gene assoc
-            try:
-                k = [i for i in list(rxn.notes.keys()) if i.lower().startswith("gene")][0]
-                gene_assoc = rxn.notes[k][0]
-                line = ["linked_gene", gene_assoc]
-            except IndexError:
-                line = ["linked_gene", ""]
+            if extract_gene:
+                try:
+                    gene_assoc = parseNotes(rxn_sbml)["GENE_ASSOCIATION"][0]
+                    line = ["linked_gene", gene_assoc]
+                except KeyError:
+                    line = ["linked_gene", ""]
+            else:
+                    line = ["linked_gene", ""]
+
             line = "\t".join(line)+"\n"
             f.write(line)
             line = ["#reactant/product","#stoichio:compound_id:compart"]
             line = "\t".join(line)+"\n"
             f.write(line)
-            reactants = rxn.reactants
-            products = rxn.products
+            reactants = rxn_sbml.getListOfReactants()
+            products = rxn_sbml.getListOfProducts()
             for reactant in reactants:
-                stoich = str(abs(rxn.get_coefficient(reactant)))
-                reactant_id = convert_from_coded_id(reactant.id)[0]
-                compart = reactant.compartment
+                stoich = str(abs(reactant.getStoichiometry()))
+                reactant_id,x, compart = convert_from_coded_id(reactant.getSpecies())
                 line = ":".join([stoich, reactant_id, compart])
                 line = "reactant"+"\t"+line+"\n"
                 f.write(line)
             for product in products:
-                stoich = str(abs(rxn.get_coefficient(product)))
-                product_id = convert_from_coded_id(product.id)[0]
-                compart = product.compartment
+                stoich = str(abs(product.getStoichiometry()))
+                product_id,x, compart = convert_from_coded_id(product.getSpecies())
                 line = ":".join([stoich, product_id, compart])
                 line = "product"+"\t"+line+"\n"
                 f.write(line)
