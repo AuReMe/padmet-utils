7465054df1296807d4195e0fc94131e58aea9b76
AITE Meziane <meziane.aite@inria.fr>
2019-06-14 18:00:55 +0200

    fix padmet_to_padmet behaviour when not using init padmet

--- padmet_utils/connection/padmet_to_padmet.py
+++ padmet_utils/connection/padmet_to_padmet.py
@@ -11,13 +11,12 @@ Description:
 ::
     
     usage:
-        padmet_to_padmet.py --to_add=FILE/DIR --output=FILE [--init_padmet=FILE] [--padmetRef=FILE]  [-v]
+        padmet_to_padmet.py --to_add=FILE/DIR --output=FILE [--padmetRef=FILE]  [-v]
     
     options:
         -h --help     Show help.
-        --to_add=FILE/DIR    path to the padmet file to add or path to folder of padmet files.
+        --to_add=FILE/DIR    path to the padmet file to add (sep: ;) or path to folder of padmet files.
         --output=FILE   path to the new padmet file
-        --init_padmet=FILE    path to the padmet file to update.
         --padmetRef=FILE    path to the padmet file representing to the database of reference (ex: metacyc_18.5.padmet)
         -v   print info
 """
@@ -32,65 +31,31 @@ def main():
         padmetRef = PadmetRef(args["--padmetRef"])
     else:
         padmetRef = None
-    if args["--init_padmet"]:
-        padmet_init = PadmetSpec(args["--init_padmet"])
-    else:
-        padmet_init = None
     output = args["--output"]
     verbose = args["-v"]
     to_add = args["--to_add"]
-    padmet_to_padmet(to_add, output, padmet_init, padmetRef, verbose)
+    padmet_to_padmet(to_add, output, padmetRef, verbose)
 
-def padmet_to_padmet(to_add, output, padmet_init=None, padmetRef=None, verbose=False):
+def padmet_to_padmet(to_add, output, padmetRef=None, verbose=False):
     """
     
     """
     if os.path.isdir(to_add):
-        padmet_type = "path"
-    elif os.path.isfile(to_add):
-        padmet_type = "file"
-    else:
-        raise TypeError("%s is not a dir or a file" %(to_add))
-
-    if padmet_type == "path":
         path = to_add
         all_files = [i for i in next(os.walk(path))[2] if not i.startswith(".~lock")]
         padmetFiles = [os.path.join(path, i) for i in all_files if i.endswith(".padmet")]
         if len(padmetFiles) == 0:
             raise IOError("No padmet found in %s" %path)
     else:
-        padmetFiles = list([to_add])
-
-
-    if not padmet_init:
-        now = datetime.now()
-        today_date = now.strftime("%Y-%m-%d")
-
-        padmet_init = PadmetSpec()
-        if padmetRef:
-            padmet_init.setInfo(padmetRef)
-            padmet_init.info["PADMET"]["creation"] = today_date
-            padmet_init.setPolicy(padmetRef)
-        else:
-            POLICY_IN_ARRAY = [['class','is_a_class','class'], ['class','has_name','name'], ['class','has_xref','xref'], ['class','has_suppData','suppData'],
-                            ['compound','is_a_class','class'], ['compound','has_name','name'], ['compound','has_xref','xref'], ['compound','has_suppData','suppData'],
-                            ['gene','is_a_class','class'], ['gene','has_name','name'], ['gene','has_xref','xref'], ['gene','has_suppData','suppData'], ['gene','codes_for','protein'],
-                            ['pathway','is_a_class','class'], ['pathway','has_name','name'], ['pathway','has_xref','xref'], ['pathway','is_in_pathway','pathway'], 
-                            ['protein','is_a_class','class'], ['protein','has_name','name'], ['protein','has_xref','xref'], ['protein','has_suppData','suppData'], ['protein','catalyses','reaction'],
-                            ['protein','is_in_species','class'], 
-                            ['reaction','is_a_class','class'], ['reaction','has_name','name'], ['reaction','has_xref','xref'], ['reaction','has_suppData','suppData'], ['reaction','has_reconstructionData','reconstructionData'], ['reaction','is_in_pathway','pathway'],  
-                            ['reaction','consumes','class','STOICHIOMETRY','X','COMPARTMENT','Y'], ['reaction','produces','class','STOICHIOMETRY','X','COMPARTMENT','Y'], 
-                            ['reaction','consumes','compound','STOICHIOMETRY','X','COMPARTMENT','Y'], ['reaction','produces','compound','STOICHIOMETRY','X','COMPARTMENT','Y'], 
-                            ['reaction','consumes','protein','STOICHIOMETRY','X','COMPARTMENT','Y'], ['reaction','produces','protein','STOICHIOMETRY','X','COMPARTMENT','Y'], 
-                            ['reaction','is_linked_to','gene','SOURCE:ASSIGNMENT','X:Y']]
-            dbNotes = {"PADMET":{"creation":today_date,"version":"2.6"},"DB_info":{"DB":'NA',"version":'NA'}}
-            padmet_init.setInfo(dbNotes)
-            padmet_init.setPolicy(POLICY_IN_ARRAY)
-
+        padmetFiles = to_add.split(";")
+    
+    padmet_init_file = padmetFiles[0]
+    padmet_init = PadmetSpec(padmet_init_file)
+    padmetFiles.pop(0)
 
     for padmet_update_file in padmetFiles:
         if verbose:
-            print("Updating %s from %s" %(os.path.basename(padmet_init),os.path.basename(padmet_update_file)))
+            print("Updating %s from %s" %(os.path.basename(padmet_init_file),os.path.basename(padmet_update_file)))
         padmet_update = PadmetSpec(padmet_update_file)
         padmet_init.updateFromPadmet(padmet_update)
 
