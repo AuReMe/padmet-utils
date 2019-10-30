7caf2c2cb7b7a77c06d7b7e81aed6db2c2c26053
AITE Meziane <meziane.aite@inria.fr>
2019-10-30 15:15:51 +0100

    fix exonerate path usage

--- padmet-utils/exploration/prot2genome.py
+++ padmet-utils/exploration/prot2genome.py
@@ -6,7 +6,7 @@ usage:
     prot2genome --query_faa=FILE --query_ids=FILE/STR --subject_gbk=FILE --subject_fna=FILE --subject_faa=FILE --output_folder=FILE --exonerate=PATH  [--cpu=INT] [blastp] [tblastn] [debug]
     prot2genome --padmet=FOLDER --output=FOLDER
     prot2genome --studied_organisms=FOLDER --output=FOLDER
-    prot2genome --run=FOLDER --exonerate=PATH  --padmetRef=FILE [--cpu=INT] [blastp] [tblastn] [debug]
+    prot2genome --run=FOLDER --padmetRef=FILE [--cpu=INT] [debug]
 
     From aucome run fromAucome():
         -1. Extract specifique reactions in spec_reactions folder with extractReactions()
@@ -21,7 +21,6 @@ options:
     --subject_faa=FILE #TODO. 
     --output_folder=FILE #TODO. 
     --cpu=INT     Number of cpu to use for the multiprocessing (if none use 1 cpu). [default: 1]
-    --exonerate=PATH #TODO. 
     blastp #TODO. 
     tblastn #TODO. 
     debug #TODO.
@@ -43,21 +42,18 @@ def main():
     """
     global exonerate_path
     args = docopt.docopt(__doc__)
-    exonerate_path = args["--exonerate"]
-    if exonerate_path:
-        print("Test running exonerate...")
-        subprocess.run([exonerate_path], shell = True)
-        exonerate = True
+
+    print("Test running exonerate...")
+    subprocess.run(["exonerate"], shell = True)
+    exonerate = True
 
     run_folder = args["--run"]
     cpu =  int(args["--cpu"])
     padmetRef = args["--padmetRef"]
-    blastp = args["blastp"]
-    tblastn = args["tblastn"]
     debug = args["debug"]
 
     if run_folder:
-        prot2genome.fromAucome(run_folder, cpu, padmetRef, blastp, tblastn, exonerate, debug)
+        prot2genome.fromAucome(run_folder, cpu, padmetRef, blastp=True, tblastn=True, exonerate=True, debug)
 
 if __name__ == "__main__":
     main()    
